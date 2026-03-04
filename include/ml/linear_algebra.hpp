2ca4d742e6f19d52d326a0a7a890899963969160
Patrick Sæbø <patrick.alfei.sabo@gmail.com>
2026-03-04 20:36:44 +0100

    Initial commit

---
+++ include/ml/linear_algebra.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cmath>
+#include <numeric>
+#include <stdexcept>
+#include <vector>
+
+namespace ml {
+class Vector {
+public:
+  std::vector<double> data;
+};
+} // namespace ml
---
+++ tests/test_linear_algebra.cpp
@@ -0,0 +1,6 @@
+#include "ml/linear_algebra.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+using namespace ml;
+using Catch::Matchers::WithinAbs;
